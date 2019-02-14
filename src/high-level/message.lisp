b886cce630b77eaea9dc9c7d83a8345946bcc881
Sahil Kang <sahil.kang@asilaycomputing.com>
2019-02-14 02:21:32 -0800

    Use correct topic field for message

--- src/high-level/message.lisp
+++ src/high-level/message.lisp
@@ -63,7 +63,7 @@
     (pointer->bytes key len)))
 
 (defun get-topic (*rd-kafka-message)
-  (let ((rd-kafka-topic (getf *rd-kafka-message 'cl-rdkafka/ll:topic)))
+  (let ((rd-kafka-topic (getf *rd-kafka-message 'cl-rdkafka/ll:rkt)))
     (cl-rdkafka/ll:rd-kafka-topic-name rd-kafka-topic)))
 
 (defmethod initialize-instance :after ((message message) &key)
