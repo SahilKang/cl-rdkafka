328f4713848e45da9a0609ff3b6f31c2cfff7983
Sahil Kang <sahil.kang@asilaycomputing.com>
2019-11-03 23:45:45 -0800

    Tidy unwind-protect in assignment and committed

--- src/high-level/consumer.lisp
+++ src/high-level/consumer.lisp
@@ -207,47 +207,34 @@ Returns nil on success or a kafka-error on failure."))
 
 (defun %assignment (rd-kafka-consumer)
   (cffi:with-foreign-object (rd-list :pointer)
-    (let ((err (cl-rdkafka/ll:rd-kafka-assignment
-                rd-kafka-consumer
-                rd-list)))
-      (if (eq err cl-rdkafka/ll:rd-kafka-resp-err-no-error)
-          (let ((*rd-list (cffi:mem-ref rd-list :pointer)))
-            (values *rd-list t))
-          (values (make-instance 'kafka-error :rd-kafka-resp-err err)
-                  nil)))))
+    (let ((err (cl-rdkafka/ll:rd-kafka-assignment rd-kafka-consumer rd-list)))
+      (unless (eq err cl-rdkafka/ll:rd-kafka-resp-err-no-error)
+        (error "~&Failed to get assignment: ~S"
+               (cl-rdkafka/ll:rd-kafka-err2str err)))
+      (cffi:mem-ref rd-list :pointer))))
 
 (defmethod assignment ((consumer consumer))
   (with-slots (rd-kafka-consumer) consumer
-    (multiple-value-bind (rd-list success?) (%assignment rd-kafka-consumer)
-      (if success?
-          (let ((topic+partitions (rd-kafka-list->topic+partitions rd-list)))
-            (cl-rdkafka/ll:rd-kafka-topic-partition-list-destroy rd-list)
-            topic+partitions)
-          (error "~&Failed to get assignment with error: ~A"
-                 (error-description rd-list))))))
-
-(defun %committed (rd-kafka-consumer rd-list)
-  (let ((err (cl-rdkafka/ll:rd-kafka-committed
-              rd-kafka-consumer
-              rd-list
-              60000)))
-    (let ((topic+partitions (rd-kafka-list->topic+partitions rd-list)))
-      (cl-rdkafka/ll:rd-kafka-topic-partition-list-destroy rd-list)
-      (unless (eq err cl-rdkafka/ll:rd-kafka-resp-err-no-error)
-        (error "~&Failed to get committed offsets with error: ~A"
-               (error-description err)))
-      topic+partitions)))
+    (let ((rd-list (%assignment rd-kafka-consumer)))
+      (unwind-protect
+           (rd-kafka-list->topic+partitions rd-list)
+        (cl-rdkafka/ll:rd-kafka-topic-partition-list-destroy rd-list)))))
 
 (defmethod committed ((consumer consumer) &optional topic+partitions)
   (with-slots (rd-kafka-consumer) consumer
-    (if topic+partitions
-        (%committed rd-kafka-consumer
-                    (topic+partitions->rd-kafka-list topic+partitions))
-        (multiple-value-bind (rd-list success?) (%assignment rd-kafka-consumer)
-          (if success?
-              (%committed rd-kafka-consumer rd-list)
-              (error "~&Failed to get committed offsets with error: ~A"
-                     (error-description rd-list)))))))
+    (let ((rd-list (if topic+partitions
+                       (topic+partitions->rd-kafka-list topic+partitions)
+                       (%assignment rd-kafka-consumer))))
+      (unwind-protect
+           (let ((err (cl-rdkafka/ll:rd-kafka-committed
+                       rd-kafka-consumer
+                       rd-list
+                       60000)))
+             (unless (eq err cl-rdkafka/ll:rd-kafka-resp-err-no-error)
+               (error "~&Failed to get committed offsets with error: ~S"
+                      (cl-rdkafka/ll:rd-kafka-err2str err)))
+             (rd-kafka-list->topic+partitions rd-list))
+        (cl-rdkafka/ll:rd-kafka-topic-partition-list-destroy rd-list)))))
 
 (defmethod assign ((consumer consumer) topic+partitions)
   (with-slots (rd-kafka-consumer) consumer
