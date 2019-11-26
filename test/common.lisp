66afe72da64c44916c99012f2939412df21b1daf
Sahil Kang <sahil.kang@asilaycomputing.com>
2019-11-25 20:28:24 -0800

    Add with-topics macro to make tests idempotent

--- test/common.lisp
+++ test/common.lisp
@@ -20,9 +20,66 @@
 (defpackage #:test
   (:use #:cl)
   (:export
-   *bootstrap-servers*))
+   *bootstrap-servers*
+   with-topics))
 
 (in-package #:test)
 
 (defparameter *bootstrap-servers* "127.0.0.1:9092"
   "Kafka bootstrap.servers that tests will use.")
+
+
+;; this doesn't always work; sometimes the delete-topic call will
+;; succeed, but the topic still persists on the cluster.
+;; TODO Change create-topic and delete-topic to accept a sequence of
+;; topics instead...that's a better api and it might fix the issue
+(defmacro with-topics ((&rest bindings) &body body)
+  "Evaluate BODY with topics optionally created and then deleted from BINDINGS.
+
+Each element in the BINDINGS list looks like either:
+  * (SYMBOL TOPIC NO-CREATE-P)
+  * (SYMBOL TOPIC)
+where SYMBOL is bound to the TOPIC string and NO-CREATE-P determines
+if the topic should be created before BODY (defaults to nil when not
+supplied).
+
+Each of the topics in BINDINGS will be deleted once BODY evaluates."
+  (when (zerop (length bindings))
+    (error "No point in using this macro without bindings."))
+  (destructuring-bind
+        (let-bindings topics-to-create)
+      (loop
+         with let-bindings = nil
+         with topics-to-create = nil
+
+         for (symbol topic no-create-p) in bindings
+
+         do (push (list symbol topic) let-bindings)
+         unless no-create-p
+         do (push symbol topics-to-create)
+
+         finally (return (list (nreverse let-bindings)
+                               (nreverse topics-to-create))))
+    (let ((client (gensym))
+          (client-form '(make-instance
+                         'kf:consumer
+                         :conf (list "bootstrap.servers" test:*bootstrap-servers*))))
+      `(let ,(cons (list client client-form)
+                   let-bindings)
+         (unwind-protect
+              (progn
+                ,@(unless (zerop (length topics-to-create))
+                    `((map nil
+                           (lambda (topic)
+                             (kf:create-topic ,client topic))
+                           (list ,@topics-to-create))
+                      (sleep 2)))
+                ,@body)
+           (progn
+             (map nil
+                  (lambda (topic)
+                    (handler-case
+                        (kf:delete-topic ,client topic)
+                      (kf:kafka-error ())))
+                  (list ,@(mapcar #'first let-bindings)))
+             (sleep 2)))))))
